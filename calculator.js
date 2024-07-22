023d131b0e4162b2d4f6ab181c6d0afb1b11c6f2
NicolasFue05 <nicolasfue05@gmail.com>
2024-07-22 00:21:12 -0500

    :feat: main constants added

---
+++ calculator.js
@@ -0,0 +1,27 @@
+// Iphone Calculator Program
+
+// Constans
+const showOperation = document.getElementById('show-operation');
+// Digits
+const Acbtn = document.getElementById('digit1');
+const plusMinusbtn = document.getElementById('digit2');
+const percentagebtn = document.getElementById('digit3');
+const divisionbtn = document.getElementById('digit4');
+const Xbtn = document.getElementById('digit5');
+const minusbtn = document.getElementById('digit6');
+const plusbtn = document.getElementById('digit7');
+const pointbtn = document.getElementById('digit8');
+// Numbers
+const number1btn = document.getElementById('number1');
+const number2btn = document.getElementById('number2');
+const number3btn = document.getElementById('number3');
+const number4btn = document.getElementById('number4');
+const number5btn = document.getElementById('number5');
+const number6btn = document.getElementById('number6');
+const number7btn = document.getElementById('number7');
+const number8btn = document.getElementById('number8');
+const number9btn = document.getElementById('number9');
+const number0btn = document.getElementById('number0');
+// Equal
+const equalbtn = document.getElementById('equal');
+
